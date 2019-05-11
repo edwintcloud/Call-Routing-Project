447c602cb4f3815a07e1f5f629a1d4543f0a9f54
Edwin Cloud <ecloud412@gmail.com>
2019-05-11 15:54:20 -0700

    Cleanup main solution :sparkles:

--- solution.py
+++ solution.py
@@ -12,7 +12,6 @@
 # ----------------------------------------------------------------------------------
 # Imports
 # ----------------------------------------------------------------------------------
-import csv
 from random import randint
 import time
 import resource
@@ -45,11 +44,6 @@ class CallRoutes(object):
         # **this is an expensive operation** but it's the best we can do
         self.numbers = self._read_numbers(numbers_file_name)
 
-        # costs will be a dictionary of dictionaries mapping the phone
-        # number to a dictionary of matched carrier costs
-        # **this is a decently fast operation**
-        self.costs = self._get_costs_for_numbers()
-
     # ------------------------------------------------------------------------------
     # CallRoutes - Intended Private Methods
     # ------------------------------------------------------------------------------
@@ -58,8 +52,8 @@ class CallRoutes(object):
         Runtime: Θ(n) Space: Θ(n)"""
         data = {}
         with open('data/' + file_name) as f:
-            csv_reader = csv.reader(f, delimiter=',')
-            for row in csv_reader:
+            for line in f:
+                row = line.strip().split(',')
                 self.route_costs += 1
                 # if the route is already in dictionary
                 # and the current route has a lower price,
@@ -75,65 +69,44 @@ class CallRoutes(object):
         with open('data/'+file_name) as f:
             return f.read().splitlines()
 
-    def _get_costs_for_numbers(self):
-        """Get costs for all numbers for each carrier. 
-        Return resulting dictionary of dictionaries.
-        Runtime: Θ(nkl) Space: Θ(n).
-        Where n is the number of numbers, k is the 
-        number of carriers, and l is the iterations
-        needed to find a match from trimming off the end."""
-        # create a result dictionary
-        result = {}
-        # iterate over phone numbers
-        for number in self.numbers:
-            # iterate for each carrier, k is the carrier name
-            # and v is the dictionary of route costs
-            for k, v in self.routes.items():
-                # trim numbers off the right side of the number until
-                # we find a match for each carrier
-                for index in range(len(number), 1, -1):
-                    # if the trimmed number is in current carrier's
-                    # route costs
-                    if number[:index] in v:
-                        # and the full phone number is not in result
-                        # dictionary
-                        if number not in result:
-                            # create new entry in result dictionary
-                            # with full phone number as the key and
-                            # a new dictionary with a single entry of
-                            # carrier name: route cost for carrier
-                            result[number] = {k: v[number[:index]]}
-                        # else if the carrier name not in the dictionary
-                        # at result[number]
-                        elif k not in result[number]:
-                            # create new entry in result[number] dictionary
-                            # equal to carrier name: route cost for carrier
-                            result[number][k] = v[number[:index]]
-                        # we found the longest matching prefix,
-                        # break from the inner loop
-                        break
-            # if route was not found for any carriers, set it's cost to 0
-            if number not in result:
-                result[number] = 0
-        # return the result dictionary
-        return result
-
     # ------------------------------------------------------------------------------
     # CallRoutes - Public Methods
     # ------------------------------------------------------------------------------
-    def get_costs(self, phone_number):
-        """Return the carrier costs dictionary for a specified phone number.
-        Runtime: Θ(1) Space: Θ(1)"""
-        try:
-            return self.costs[phone_number]
-        except KeyError:
-            return "Key not found!"
+
+    def get_costs(self, number):
+        """Return costs from each carrier for a number.
+        Runtime: Θ(nk) Space: Θ(n).
+        Where n is the number of carriers, and k is the 
+        iterations needed to find a match from trimming off 
+        the end."""
+        # create a result list
+        result = []
+        # iterate for each carrier, k is the carrier name
+        # and v is the dictionary of route costs
+        for k, v in self.routes.items():
+            # trim numbers off the right side of the number until
+            # we find a match for each carrier
+            for index in range(len(number), 1, -1):
+                # if the trimmed number is in current carrier's
+                # route costs
+                if number[:index] in v:
+                    # append a tuple of carrier, cost to result
+                    # list
+                    result.append((k, v[number[:index]]))
+                    # we found the longest matching prefix,
+                    # break from the inner loop
+                    break
+        # if route was not found for any carriers, return 0
+        if len(result) == 0:
+            return 0
+        # return the result list
+        return result
 
     def yield_costs(self):
-        """Return an iterator to iterate over each phone number in costs.
-        Runtime: Θ(1) Space: Θ(1)"""
-        for cost in self.costs.items():
-            yield cost
+        """Return an iterator to get the costs of each number in self.numbers.
+        Runtime: Θ(nk) Space: Θ(1)"""
+        for number in self.numbers:
+            yield "{} : {}".format(number, self.get_costs(number))
 
 # ------------------------------------------------------------------------------
 # Memory Usage Function
@@ -154,8 +127,10 @@ def get_mem():
 # Main Entry Point
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
+    # start the timer
     start = time.time()
     print("\nInitializing please wait...")
+    # initialize the CallRoutes class with some data
     calls = CallRoutes("phone-numbers-10000.txt",
                        ("carrierA", "route-costs-3.txt"),
                        ("carrierB", "route-costs-10.txt"),
@@ -165,10 +140,13 @@ if __name__ == '__main__':
                        ("carrierF", "route-costs-106000.txt"),
                        ("carrierG", "route-costs-1000000.txt"),
                        ("carrierH", "route-costs-10000000.txt"))
+    # calculate and print load time and memory
     load_time = round(time.time()-start, 4)
     print("\nInitialized {:,} route costs in {} seconds.".format(calls.route_costs,
                                                                  load_time))
     get_mem()
+
+    # Main Loop
     while(True):
         print("\n==========================================")
         print("|              Main Menu                 |")
@@ -180,10 +158,12 @@ if __name__ == '__main__':
         print("4. Display load time and memory statistics.")
         print("5. Exit the program.")
         choice = input("\nPlease enter a number: ")
+        # ensure choice is a number, or break
         try:
             choice = int(choice)
         except Exception:
             break
+        # Option 1
         if choice == 1:
             start = time.time()
             for _ in range(100):
@@ -192,10 +172,12 @@ if __name__ == '__main__':
                     calls.numbers[idx], calls.get_costs(calls.numbers[idx])))
             print("\nCompleted in {} seconds.".format(
                 round(time.time()-start, 4)))
+        # Option 2
         elif choice == 2:
             new_number = input("\nEnter full number with prefix: ")
             print("\n{} : {}".format(
                 new_number, calls.get_costs(new_number)))
+        # Option 3
         elif choice == 3:
             costs_gen = calls.yield_costs()
             while(True):
@@ -204,10 +186,13 @@ if __name__ == '__main__':
                 if opt == "q":
                     break
                 print("\n{}".format(next(costs_gen)))
+        # Option 4
         elif choice == 4:
             print("\n{:,} route costs were loaded in {} seconds.".format(calls.route_costs,
                                                                          load_time))
             get_mem()
+        # Option 5
         else:
             break
+    # cheerio
     print("\nGoodbye!")
